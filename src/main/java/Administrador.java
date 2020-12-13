62b64ddb2dd8e92cb415c28d93d308cfffe86021
josea <josea@192.168.1.71>
2020-12-12 19:27:14 -0600

    Se agrego la variable String "nomAdmin" en la clase Administrador

--- src/main/java/Administrador.java
+++ src/main/java/Administrador.java
@@ -1,9 +1,12 @@
 
 public class Administrador extends Tienda {
+    
+    String nomAdmin;
    
     public Administrador(String productos, String nacion, int cant, double percio) {
         super(productos, nacion, cant, percio);
     }
    
+    
     
 }
